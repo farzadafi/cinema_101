c7a2fbd9e64c5aa3bbabbb18197ca1ec9d1e3fb6
HoseinRezaeeM <rezaeen77@gmail.com>
2023-10-08 22:33:44 +0330

    Write Constructor

--- src/main/java/org/cinema/Person.java
+++ src/main/java/org/cinema/Person.java
@@ -6,5 +6,10 @@ public class Person {
     private String userName;
     private String password;
 
-
+    public Person(String firstname, String lastName, String userName, String password) {
+        this.firstname = firstname;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+    }
 }
