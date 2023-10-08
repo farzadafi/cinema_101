d5c6055da8deec52180ccdbdcc0a0aa8e74e89a2
DaniRoses <147339021+AmirTghizde@users.noreply.github.com>
2023-10-08 23:05:32 +0330

    Added lombok codes to Person and made it extend from BaseEntity

--- src/main/java/org/cinema/Person.java
+++ src/main/java/org/cinema/Person.java
@@ -1,10 +1,16 @@
 package org.cinema;
 
-public class Person {
-    protected String firstname;
-    protected String lastName;
+import lombok.*;
+import org.cinema.base.model.BaseEntity;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person<ID extends Serializable> extends BaseEntity<ID> {
+    private String firstname;
+    private String lastName;
     private String userName;
     private String password;
-
-
 }
--- src/main/java/org/cinema/base/model/BaseEntity.java
+++ src/main/java/org/cinema/base/model/BaseEntity.java
@@ -2,11 +2,13 @@ package org.cinema.base.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 @Getter
 @Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @SuppressWarnings("unused")
 public class BaseEntity<ID extends Serializable> {
