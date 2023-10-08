e899b02547268ab76f2e3deff6c149f8ae8ef78a
DaniRoses <147339021+AmirTghizde@users.noreply.github.com>
2023-10-08 22:36:13 +0330

    Added base entity

---
+++ src/main/java/org/cinema/base/model/BaseEntity.java
@@ -0,0 +1,14 @@
+package org.cinema.base.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+@Getter
+@Setter
+@AllArgsConstructor
+@SuppressWarnings("unused")
+public class BaseEntity<ID extends Serializable> {
+    private ID id;
+}
