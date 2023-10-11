75f49e82f3524db6073aeb8149f21d578bfd377c
sahargholami1992 <sahar_gholami03@gmail.com>
2023-10-11 14:09:14 +0330

    edit sql command in basket repository and change type of username

--- src/main/java/org/cinema/entity/Basket.java
+++ src/main/java/org/cinema/entity/Basket.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Basket {
-    private String username;
+    private Users username;
     private Integer idTicket;
     private String filmName;
     private Integer number;
--- src/main/java/org/cinema/repository/BasketRepository.java
+++ src/main/java/org/cinema/repository/BasketRepository.java
@@ -1,6 +1,6 @@
-package org.cnema;
+package org.cinema.repository;
 
-import org.cinema.Basket;
+import org.cinema.entity.Basket;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -8,23 +8,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class BasketRepository {
-    private Connection connectio;
+    private Connection connection;
 
     public BasketRepository(Connection connection) throws SQLException {
         this.connection = connection;
-        String createTable = " CREATE TABLE IF NOT EXISTS asket(id serial,username varchar(50) REFERENCES UserTable(username)," +
-                       "idTicket Integer REFERENCES TicketTable(id),filmNae varchar(0),numberTicket Integer,priceAll Integer) ";
+        String createTable = " CREATE TABLE IF NOT EXISTS basket(id serial,username varchar(50) REFERENCES UserTable(username)," +
+                       "idTicket Integer REFERENCES TicketTable(id),filmName varchar(50),numberTicket Integer,priceAll Integer) ";
         PreparedStatement preparedStatement = connection.prepareStatement(createTable);
         preparedStatement.execute();
     }
 
     //::::>
     public int importTicket(Basket basket) throws SQLException {
-        String importBasket = "INSERT INTO (username,idTicket,filmName,numberTicket,pricea) VALUES (?,?, ?, ?)";
+        String importBasket = "INSERT INTO basket(username,idTicket,filmName,numberTicket,pricea) VALUES (?, ?, ?, ?, ?)";
         PreparedStatement preparedStatement = connection.prepareStatement(importBasket);
-        preparedStatement.setString(1,basket.getUsername());
+        preparedStatement.setString(1,basket.getUsername().getUserName());
         preparedStatement.setInt(2,basket.getIdTicket());
-        preparedStatement.setString(5,basket.getFilmName());
+        preparedStatement.setString(3,basket.getFilmName());
         preparedStatement.setInt(4,basket.getNumber());
         preparedStatement.setInt(5,basket.getPriceAll());
         return preparedStatement.executeUpdate();
@@ -32,7 +32,7 @@ public class BasketRepository {
 
     //::::>
     public void cancelTicket(Integer id) throws SQLException {
-        String cancel = "DELETE FROM Basket WHERE iTicket = ? ";
+        String cancel = "DELETE FROM basket WHERE iTicket = ? ";
         PreparedStatement preparedStatement = connection.prepareStatement(cancel);
         preparedStatement.setInt(1,id);
         preparedStatement.executeUpdate();
@@ -40,12 +40,12 @@ public class BasketRepository {
 
     //::::>
     public void viewMyBasket(String username) throws SQLException {
-        String finduser = " SELECT * Basket WHERE username = ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(finduser);
+        String findUser = " SELECT * basket WHERE username = ? ";
+        PreparedStatement preparedStatement = connection.prepareStatement(findUser);
         preparedStatement.setString(1,username);
         ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
-                System.out.println("id=" + resultSet.getInt("id") "  |filmName="  resultSet.getString("filmName") +
+                System.out.println("id=" + resultSet.getInt("id")+ "  |filmName=" + resultSet.getString("filmName") +
                                    "   |numberTicket=" + resultSet.getInt("numberTicket") + "   |priceAll=" + resultSet.getInt("priceall"));
 
             }
