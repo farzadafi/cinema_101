a8552da09de825246f3f6da68bad67cea6a22996
DaniRoses <147339021+AmirTghizde@users.noreply.github.com>
2023-10-15 23:43:37 +0330

    Added lombok to Cinema, Ticket and users classes

--- src/main/java/org/cinema/entity/Ticket.java
+++ src/main/java/org/cinema/entity/Ticket.java
@@ -1,6 +1,9 @@
 package org.cinema.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.cinema.base.model.BaseEntity;
 
 import java.io.Serializable;
@@ -8,6 +11,9 @@ import java.sql.Date;
 import java.sql.Time;
 @SuppressWarnings("unused")
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ticket<ID extends Serializable> extends BaseEntity<ID> {
     private String cinemaName;
     private String filmName;
--- src/main/java/org/cinema/entity/Users.java
+++ src/main/java/org/cinema/entity/Users.java
@@ -1,12 +1,15 @@
 package org.cinema.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.io.Serializable;
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users<ID extends Serializable> extends Person<ID>{
-
-
-    public Users(String firstname, String lastName, String userName, String password) {
-        super(firstname, lastName, userName, password);
-    }
 
 }
