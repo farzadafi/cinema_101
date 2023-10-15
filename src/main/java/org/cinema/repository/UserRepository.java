dba143898fa1eb7b25b4f41b3f7322babb2d5c28
DaniRoses <147339021+AmirTghizde@users.noreply.github.com>
2023-10-15 23:53:31 +0330

    Fixed UserRepository's name

--- src/main/java/org/cinema/repository/UserRepository.java
+++ src/main/java/org/cinema/repository/UserRepository.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserRepositoryis {
+public class UserRepository {
     private Connection connection;
 
 
