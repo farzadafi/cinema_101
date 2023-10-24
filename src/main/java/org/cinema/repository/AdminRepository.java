3554ea9fc8bd566639b4cf2eb4e3d55cb5162402
AmirTghizde <a.taghizadeh@gmail.com>
2023-10-24 22:10:08 +0330

    Changed AdminRepository class into Interface

--- src/main/java/org/cinema/repository/AdminRepository.java
+++ src/main/java/org/cinema/repository/AdminRepository.java
@@ -1,44 +1,12 @@
 package org.cinema.repository;
 
 
+import org.cinema.base.repository.BaseEntityRepository;
 import org.cinema.entity.Admin;
 
 import java.sql.*;
 
 @SuppressWarnings("unused")
-public class AdminRepository {
-    private Connection connection;
-
-
-    public AdminRepository(Connection connection) throws SQLException {
-        this.connection = connection;
-    }
-
-
-
-
-    public int importAdmin(Admin admin) throws SQLException {
-        String importValue = "INSERT INTO Admin(firstName,lastName,username,password) VALUES (?,?,?,?)";
-        PreparedStatement preparStatement = connection.prepareStatement(importValue);
-        preparedStatement.setString(1, admin.getFirstName());
-        preparedStatement.setString(2, admin.getLastName());
-        preparedStatement.setString(3, admin.getUserName());
-        preparedStatement.setString(4, admin.getPassword());
-        return preparedStatement.executeUpdate();
-    }
-
-    //::::>
-    public String findAdmin(String username, String password) throws SQLException {
-        String findQuery = "SELECT * FRO Admin WHERE usernam = ? AND password = ?, ? ";
-        PreparedStatement preparedStatement = connection.prepareStatement(findQuery);
-        preparedStatement.setString(2, username);
-        preparedStatement.setString(3, password);
-        ResultSet resultSet = preparedStatement.executeQuery();
-        if (resultSet.next())
-            return resultSet.getString("usernam");
-        else
-            return null;
-    }
-
+public interface AdminRepository extends BaseEntityRepository<Integer,Admin<Integer>> {
 
 }
